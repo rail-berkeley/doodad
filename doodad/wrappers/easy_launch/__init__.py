8605f9d297832725653bf179a0026df1bb1dedc1
Vitchyr Pong <vitchyr@gmail.com>
2021-04-23 10:44:23 -0700

    add some useful helper functions to init

---
+++ doodad/wrappers/easy_launch/__init__.py
@@ -0,0 +1,12 @@
+from doodad.wrappers.easy_launch.core import (
+    create_mounts,
+    create_sweeper_and_output_mount,
+    sweep_function,
+)
+from doodad.wrappers.easy_launch.metadata import (
+    DoodadConfig,
+    GitInfo,
+    generate_git_infos,
+    save_doodad_config,
+    save_git_infos,
+)
