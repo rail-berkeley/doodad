2f895c57107cc7a786c02964d476a1cbe09058da
Vitchyr Pong <vitchyr@gmail.com>
2021-05-01 16:39:54 -0700

    Add MountRemote for custom mounts (#15)
    * add MountRemote for custom mounts
    
    * add docstring to mount remote

--- doodad/mount.py
+++ doodad/mount.py
@@ -263,3 +263,25 @@ class MountAzure(Mount):
     def dar_extract_command(self):
         return 'echo helloMountAzure'
 
+
+class MountRemote(Mount):
+    """This is for mounting writable directories that you know will already
+    exist on whatever platform you're using."""
+
+    def __init__(self, local_dir=None, output=True, **kwargs):
+        """Create a remote mount.
+
+        :param local_dir: a directory that you know will exist locally on the
+        running machine
+        :param output: same meaning as in `Mount`, but default to True.
+        :param kwargs: kwargs for `Mount`
+        """
+        super(MountRemote, self).__init__(output=output, **kwargs)
+        self._name = local_dir
+        self.sync_dir = local_dir
+
+    def dar_build_archive(self, deps_dir):
+        return
+
+    def dar_extract_command(self):
+        return 'echo helloMountRemote'
